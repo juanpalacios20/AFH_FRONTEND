30b64b76173f80484fc3a2d524b0fefd5b0ded02
juanpalacios20 <juan.palacios.casanova@correounivalle.edu.co>
2025-03-29 14:08:37 -0500

    Refactor componentes de menú y herramientas de gestión, integrando el menú como componente independiente y simplificando la lógica de visibilidad del drawer.

--- afh_frontend/src/app/shared/ui/menu/menu.component.ts
+++ afh_frontend/src/app/shared/ui/menu/menu.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { StyleClass } from 'primeng/styleclass';
+import { Drawer } from 'primeng/drawer';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  standalone: true,
+  imports: [DrawerModule, ButtonModule, Ripple, AvatarModule, StyleClass, RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  @ViewChild('drawerRef') drawerRef!: Drawer;
+  @Input() visible: boolean = false;
 
+  closeCallback(e: Event): void {
+    this.drawerRef.close(e);
+  }
+
+  toggleDrawer() {
+    this.visible = !this.visible;
+  }
+
+  closeDrawer() {
+    this.visible = false;
+  }
 }
--- afh_frontend/src/app/tools/ui/management-tools/management-tools.component.ts
+++ afh_frontend/src/app/tools/ui/management-tools/management-tools.component.ts
@@ -1,25 +1,15 @@
-import { Component, ViewChild } from '@angular/core';
-import { DrawerModule } from 'primeng/drawer';
+import { Component} from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { Ripple } from 'primeng/ripple';
-import { AvatarModule } from 'primeng/avatar';
-import { StyleClass } from 'primeng/styleclass';
-import { Drawer } from 'primeng/drawer';
-import { RouterLink } from '@angular/router';
+import { RippleModule } from 'primeng/ripple';
+import { MenuComponent } from '../../../shared/ui/menu/menu.component';
 
 @Component({
   selector: 'app-management-tools',
   standalone: true,
-  imports: [DrawerModule, ButtonModule, Ripple, AvatarModule, StyleClass, RouterLink],
+  imports: [ButtonModule, RippleModule, MenuComponent],
   templateUrl: './management-tools.component.html',
   styleUrl: './management-tools.component.css',
 })
 export class ManagementToolsComponent {
-  @ViewChild('drawerRef') drawerRef!: Drawer;
 
-  closeCallback(e: Event): void {
-    this.drawerRef.close(e);
-  }
-
-  visible: boolean = false;
 }
