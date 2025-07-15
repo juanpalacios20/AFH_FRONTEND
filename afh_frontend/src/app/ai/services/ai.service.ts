82f085a47d9a1595791511d8e595f3e79813b7cc
juanpalacios20 <juan.palacios.casanova@correounivalle.edu.co>
2025-07-15 14:53:41 -0500

    Refactor AIService and improve reports component layout with enhanced filter options and formatting

--- afh_frontend/src/app/ai/services/ai.service.ts
+++ afh_frontend/src/app/ai/services/ai.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { BaseHttpService } from "../../shared/data_access/base_http.service";
-import { environment } from "../../environments/environment.development";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { environment } from "../../environments/environment.development";
 
 @Injectable({
   providedIn: 'root',
