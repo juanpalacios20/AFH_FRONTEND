3c922b6f80a2dc6b9b6e954f6229d69974c0295a
juanpalacios20 <juan.palacios.casanova@correounivalle.edu.co>
2025-07-23 08:41:45 -0500

    fixture env

--- afh_frontend/src/app/environments/environment.ts
+++ afh_frontend/src/app/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-    production: false,
-    API_URL: '',
-    API_KEY: '',
+  API_URL: 'http://127.0.0.1:8000/',
+  API_KEY: 'AIzaSyC-rSofVvzX0gLa_y12xgpLM6A1iIkoxW4',
 };
-
