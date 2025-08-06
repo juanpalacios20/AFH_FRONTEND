7883ea119a75fa3768f14905058992ab646d0b9d
juanpalacios20 <juan.palacios.casanova@correounivalle.edu.co>
2025-08-06 13:41:43 -0500

    resolve problems with environment variables

--- afh_frontend/generate-env.js
+++ afh_frontend/generate-env.js
@@ -1,20 +1,24 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // Ruta correcta al environment.ts
-const targetPath = path.join(__dirname, './src/app/environments/environment.ts');
+const targetPath = path.join(
+  __dirname,
+  "./src/app/environments/environment.ts"
+);
 
 // Construir el contenido con las variables de Vercel
 const envConfig = `
 export const environment = {
   production: true,
   API_URL: '${process.env.NG_APP_API_URL}',
-  API_KEY: '${process.env.NG_APP_API_KEY}'
+  API_KEY: '${process.env.NG_APP_API_KEY}',
+  APP_KEY: '${process.env.NG_APP_APP_KEY}',
+  CLUSTER: '${process.env.NG_APP_CLUSTER}'
 };
 `;
 
 // Escribir el archivo
 fs.writeFileSync(targetPath, envConfig);
 
-console.log('✅ environment.ts generado con las variables de entorno.');
-
+console.log("✅ environment.ts generado con las variables de entorno.");
