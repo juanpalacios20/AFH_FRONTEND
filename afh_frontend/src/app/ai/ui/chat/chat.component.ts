d2eaed85b1be288ba9ceb0bb23e2e3d5c184179f
juanpalacios20 <juan.palacios.casanova@correounivalle.edu.co>
2025-08-28 10:14:22 -0500

    finalizando cambios

--- afh_frontend/src/app/ai/ui/chat/chat.component.ts
+++ afh_frontend/src/app/ai/ui/chat/chat.component.ts
@@ -1,15 +1,30 @@
-import { AfterViewChecked, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { AIService } from '../../services/ai.service';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
 import { MenuComponent } from '../../../shared/ui/menu/menu.component';
 import { CommonModule } from '@angular/common';
-import {  MarkdownModule } from 'ngx-markdown';
+import { MarkdownModule } from 'ngx-markdown';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-chat',
-  imports: [FloatLabelModule, InputTextModule, FormsModule, MenuComponent, CommonModule, FormsModule,  MarkdownModule],
+  imports: [
+    FloatLabelModule,
+    InputTextModule,
+    FormsModule,
+    MenuComponent,
+    CommonModule,
+    FormsModule,
+    MarkdownModule,
+    Button,
+  ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css',
 })
@@ -18,17 +33,17 @@ export default class ChatComponent implements AfterViewChecked {
   userInput: string = '';
   loading: boolean = false;
 
-  
   @ViewChild('chatContainer') chatContainer!: ElementRef;
 
   constructor(private aiService: AIService) {}
-   ngAfterViewChecked() {
+  ngAfterViewChecked() {
     this.scrollToBottom();
   }
 
   scrollToBottom() {
     try {
-      this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+      this.chatContainer.nativeElement.scrollTop =
+        this.chatContainer.nativeElement.scrollHeight;
     } catch {}
   }
 
@@ -40,13 +55,20 @@ export default class ChatComponent implements AfterViewChecked {
     this.userInput = '';
 
     this.aiService.sendPrompt(userMessage).subscribe({
-      next: (response: { candidates?: { content?: { parts?: { text?: string }[] } }[] } ) => {
-        const aiResponse = response?.candidates?.[0]?.content?.parts?.[0]?.text || 'Error al procesar';
+      next: (response: {
+        candidates?: { content?: { parts?: { text?: string }[] } }[];
+      }) => {
+        const aiResponse =
+          response?.candidates?.[0]?.content?.parts?.[0]?.text ||
+          'Error al procesar';
         this.messages.push({ role: 'ai', content: aiResponse });
       },
       error: () => {
-        this.messages.push({ role: 'ai', content: 'Ocurrió un error al conectarse con Gemini' });
-      }
+        this.messages.push({
+          role: 'ai',
+          content: 'Ocurrió un error al conectarse con Gemini',
+        });
+      },
     });
   }
 }
